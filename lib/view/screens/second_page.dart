a7a63ec12045242f8eb620f8f18bd94cd0f80666
acrobat0 <imsinghshivnedra@gmail.com>
2024-01-31 17:06:23 +0530

    Clock App

--- lib/view/screens/second_page.dart
+++ lib/view/screens/second_page.dart
@@ -29,35 +29,35 @@ class _SecondPageState extends State<SecondPage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: [
-            // LinearProgressIndicator(
-            //   color: Colors.green,
-            //   backgroundColor: Colors.grey.withOpacity(0.4),
-            //   minHeight: 12,
-            //   // value: progress,
-            //   borderRadius: const BorderRadius.all(
-            //     Radius.circular(50),
-            //   ),
-            // ),
-            //  CircularProgressIndicator(
-            //   color: Colors.blue,
-            //   backgroundColor: Colors.white,
-            //   strokeWidth: 4,
-            //   value: progress,
-            // ),
-            // Transform.scale(
-            //   // scale: 1.5,
-            //   scaleX: 1,
-            //   scaleY: 1.5,
-            //   child: CircularProgressIndicator(),
-            // ),
-            // Transform.translate(
-            //   offset: const Offset(150, -300),
-            //   child: Container(
-            //     height: 50,
-            //     width: 50,
-            //     color: Colors.blue,
-            //   ),
-            // ),
+            LinearProgressIndicator(
+              color: Colors.green,
+              backgroundColor: Colors.grey.withOpacity(0.4),
+              minHeight: 12,
+              // value: progress,
+              borderRadius: const BorderRadius.all(
+                Radius.circular(50),
+              ),
+            ),
+             CircularProgressIndicator(
+              color: Colors.blue,
+              backgroundColor: Colors.white,
+              strokeWidth: 4,
+              value: progress,
+            ),
+            Transform.scale(
+              // scale: 1.5,
+              scaleX: 1,
+              scaleY: 1.5,
+              child: CircularProgressIndicator(),
+            ),
+            Transform.translate(
+              offset: const Offset(150, -300),
+              child: Container(
+                height: 50,
+                width: 50,
+                color: Colors.blue,
+              ),
+            ),
             Transform.rotate(
               angle: pi /5,
               child: Text(
